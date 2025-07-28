1316a98a8d5e9b1cb0de5c8fc0273519cf13a8c9
Daniel Magnus <dannimagn@gmail.com>
2025-07-28 22:26:18 +0200

    Made it so when you click the cards, they can also expand

--- script.js
+++ script.js
@@ -11,12 +11,35 @@ kvb.addEventListener('click', () => {
     kvbBox.style.maxHeight = 'fit-content';
     moreKVB.style.visibility = 'visible';
     kvb.textContent = '- show less'
+    kvbBox.style.boxShadow = "0 0 20px rgb(207, 185, 233)";
+
 
     }
     else {
         kvbBox.style.maxHeight = '';
         kvb.textContent = '+ show more'
         moreKVB.style.visibility = 'hidden';
+        kvbBox.style.boxShadow = "";
+
+    };
+});
+
+kvbBox.addEventListener('click', () => {
+
+    if (kvb.textContent === '+ show more') {
+    kvbBox.style.maxHeight = 'fit-content';
+    moreKVB.style.visibility = 'visible';
+    kvb.textContent = '- show less'
+    kvbBox.style.boxShadow = "0 0 20px rgb(207, 185, 233)";
+
+
+    }
+    else {
+        kvbBox.style.maxHeight = '';
+        kvb.textContent = '+ show more'
+        moreKVB.style.visibility = 'hidden';
+        kvbBox.style.boxShadow = "";
+
     };
 });
 
@@ -26,11 +49,32 @@ rafal.addEventListener('click', () => {
     rafalBox.style.maxHeight = 'fit-content';
     moreRafal.style.visibility = 'visible';
     rafal.textContent = '- show less'
+    rafalBox.style.boxShadow = '0 0 20px rgb(255, 148, 112)';
 
     }
     else {
         rafalBox.style.maxHeight = '';
         rafal.textContent = '+ show more'
         moreRafal.style.visibility = 'hidden';
+        rafalBox.style.boxShadow = "";
+
+    };
+});
+
+rafalBox.addEventListener('click', () => {
+
+    if (rafal.textContent === '+ show more') {
+    rafalBox.style.maxHeight = 'fit-content';
+    moreRafal.style.visibility = 'visible';
+    rafal.textContent = '- show less'
+    rafalBox.style.boxShadow = '0 0 20px rgb(255, 148, 112)';
+
+    }
+    else {
+        rafalBox.style.maxHeight = '';
+        rafal.textContent = '+ show more'
+        moreRafal.style.visibility = 'hidden';
+        rafalBox.style.boxShadow = "";
+
     };
 });
